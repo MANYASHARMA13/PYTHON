f77fcc8bdfb09e7929a2eddc521d7cdb27dcced6
MANYASHARMA13 <104287551+MANYASHARMA13@users.noreply.github.com>
2022-07-03 22:20:03 +0530

    Add files via upload

---
+++ Assignment 6 output.py
@@ -0,0 +1,69 @@
+Python 3.10.4 (tags/v3.10.4:9d38120, Mar 23 2022, 23:13:41) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+= RESTART: C:/Users/Manya/AppData/Local/Programs/Python/Python310/Assignment 6 input.py
+Enter a number: 12
+12  is not a perfect number
+
+Enter a String: madam
+madam is a palindrome
+
+Enter a number: 12
+             1 
+            1 1 
+           1 2 1 
+          1 3 3 1 
+         1 4 6 4 1 
+        1 5 10 10 5 1 
+       1 6 15 20 15 6 1 
+      1 7 21 35 35 21 7 1 
+     1 8 28 56 70 56 28 8 1 
+    1 9 36 84 126 126 84 36 9 1 
+   1 10 45 120 210 252 210 120 45 10 1 
+  1 11 55 165 330 462 462 330 165 55 11 1 
+
+Enter a String: hey! how are you?
+No 'hey! how are you?' is not a panagram
+
+Enter a hyphen separated sentence: green-red-yellow-black-white
+black-green-red-white-yellow
+
+Student ID= 21102029
+Student ID= 21102029
+Student Name= Manya
+Student ID= 21102029
+Student Name= 21102029
+Student Class= 1st Year
+
+True
+False
+True
+False
+
+Check whether the said classes are subclasses of the built-in object class or not.
+True
+True
+
+Enter number of elements : 10
+-5
+-5
+8
+-5
+-3
+2
+12
+8
+10
+-2
+-5 -5 10
+-5 8 -3
+-5 -5 10
+-5 -3 8
+-5 8 -3
+-5 -5 10
+-5 -3 8
+8 -5 -3
+-5 -3 8
+None
+Enter the sequence of parantheses : {]
+{] - is unbalanced
